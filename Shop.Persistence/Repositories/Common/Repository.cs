d485fee5b273b9c7f3ec4ab64bfe45a23289739c
Sina SayaSystem <sina121gh@gmail.com>
2025-08-03 11:42:25 +0330

    added IRepository and implementaion

---
+++ Shop.Persistence/Repositories/Common/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Interfaces.Common;
+using Shop.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.Repositories.Common
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ShopDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ShopDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+        public void Update(T entity) => _dbSet.Update(entity);
+        public void Delete(T entity) => _dbSet.Remove(entity);
+    }
+}
