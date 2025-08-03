5efdd9f96993b9bef26f8991f2e97256b98318e4
Sina SayaSystem <sina121gh@gmail.com>
2025-08-03 11:51:58 +0330

    added product repository

---
+++ Shop.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Domain.Entities;
+using Shop.Domain.Interfaces.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+    }
+}
---
+++ Shop.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Domain.Entities;
+using Shop.Domain.Interfaces;
+using Shop.Persistence.Context;
+using Shop.Persistence.Repositories.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ShopDbContext _context;
+
+        public ProductRepository(ShopDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
