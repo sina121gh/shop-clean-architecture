3ad057d400ab704bd270660776d824ced0d08d27
Sina SayaSystem <sina121gh@gmail.com>
2025-08-03 10:33:12 +0330

    added db context

---
+++ Shop.Persistence/Context/ShopDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.Context
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
