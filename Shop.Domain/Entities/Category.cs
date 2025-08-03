9c0d164570c28b3537684bedb5ba8e740a80a970
Sina SayaSystem <sina121gh@gmail.com>
2025-08-03 10:29:02 +0330

    added product and category entities

---
+++ Shop.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ Shop.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using Shop.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
