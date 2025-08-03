4a452ff7dcf1693413abcdb21818e738dedc4551
Sina SayaSystem <sina121gh@gmail.com>
2025-08-03 11:06:16 +0330

    added configuration for entities

---
+++ Shop.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
---
+++ Shop.Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(p => p.Price)
+                .IsRequired();
+
+            builder.Property(p => p.IsActive)
+                .IsRequired();
+
+            builder.HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+        }
+    }
+}
