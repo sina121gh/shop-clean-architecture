ac96f080c7d93f5cddae7d58d3b878c68929d971
Sina SayaSystem <sina121gh@gmail.com>
2025-08-03 12:03:27 +0330

    added products controller

---
+++ Shop.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Domain.Entities;
+using Shop.Domain.Interfaces;
+
+namespace Shop.API.Controllers
+{
+    [Route("products")]
+    public class ProductsController : ControllerBase
+    {
+        #region Ctor
+
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        #endregion
+
+        public async Task<ActionResult> GetAllAsync()
+        {
+            return Ok(await _productRepository.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductByIdAsync(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+
+            if (product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
