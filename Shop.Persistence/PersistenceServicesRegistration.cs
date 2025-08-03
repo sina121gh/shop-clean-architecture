97be0f8e329a48db83c1e7eede2e5b961a82c53f
Sina SayaSystem <sina121gh@gmail.com>
2025-08-03 11:16:22 +0330

    added persistence services registration

---
+++ Shop.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ShopDbContext>(options =>
+            {
+                options.UseSqlServer("");
+            });
+
+            return services;
+        }
+    }
+}
