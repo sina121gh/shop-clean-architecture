be076791f9e61a220b834cb13eaf95a109397e8d
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-07 10:27:08 +0330

    added some exceptions

---
+++ Shop.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+
+namespace Shop.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public List<string> Errors { get; set; } = new();
+
+        public ValidationException(FluentValidation.Results.ValidationResult validationResult)
+        {
+            validationResult.Errors.ForEach(err => Errors.Add(err.ErrorMessage));
+        }
+    }
+}
