fa729b977a7a2af40ca6e70e95087f5e1205a7a5
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-17 10:56:33 +0330

    added delete user command and handler

---
+++ Shop.Application/Features/Users/Commands/Delete/DeleteUser.cs
@@ -0,0 +1,47 @@
+﻿using ErrorOr;
+using MapsterMapper;
+using MediatR;
+using Shop.Application.Contracts.Persistence;
+using Shop.Application.Features.Users.Queries.GetById;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Commands.Delete
+{
+    public class DeleteUserCommand : IRequest<ErrorOr<Unit>>
+    {
+        public int Id { get; set; }
+    }
+
+    class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, ErrorOr<Unit>>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<ErrorOr<Unit>> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+            if (user == null)
+                return Error.NotFound($"کاربر با آیدی {request.Id} یافت نشد");
+
+            _userRepository.Delete(user);
+
+            try
+            {
+                await _userRepository.SaveChangesAsync();
+                return Unit.Value;
+            }
+            catch (Exception)
+            {
+                return Error.Unexpected("خطایی در ثبت اطلاعات رخ داد");
+            }
+        }
+    }
+}
---
+++ Shop.Application/Features/Users/Commands/Delete/DeleteUserCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Shop.Application.Contracts.Persistence;
+using Shop.Application.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Commands.Delete
+{
+    public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        public DeleteUserCommandValidator(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("آیدی را وارد کنید")
+                .MustAsync(async (id, token) => await _userRepository.DoesExistByIdAsync(id))
+                .WithMessage("کاربر یافت نشد").WithErrorCode("404");
+        }
+    }
+}
