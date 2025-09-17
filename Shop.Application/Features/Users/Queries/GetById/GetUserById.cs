b849542817ef45b95d49515c30cd6282da5ebc17
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-17 09:23:53 +0330

    added get user by id query and handler

---
+++ Shop.Application/Features/Users/Queries/GetById/GetUserById.cs
@@ -0,0 +1,40 @@
+﻿using ErrorOr;
+using MapsterMapper;
+using MediatR;
+using Shop.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Queries.GetById
+{
+    public class GetUserByIdQuery : IRequest<ErrorOr<ShowUserDto>>
+    {
+        public int Id { get; set; }
+    }
+
+
+    class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, ErrorOr<ShowUserDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserByIdQueryHandler(IUserRepository userRepository,
+            IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ErrorOr<ShowUserDto>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+            if (user == null)
+                return Error.NotFound($"کاربر با آیدی {request.Id} یافت نشد");
+
+            return _mapper.Map<ShowUserDto>(user);
+        }
+    }
+}
---
+++ Shop.Application/Features/Users/Queries/GetById/ShowUserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Queries.GetById
+{
+    public record ShowUserDto
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
