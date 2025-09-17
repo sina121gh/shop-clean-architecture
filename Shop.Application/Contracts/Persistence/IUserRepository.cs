1bd658945be2ef41adc13c404c60f5429de08f67
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-17 09:02:56 +0330

    added user repository

---
+++ Shop.Application/Contracts/Persistence/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Application.Contracts.Persistence.Common;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Contracts.Persistence
+{
+    public interface IUserRepository : IRepository<User>
+    {
+    }
+}
---
+++ Shop.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Application.Contracts.Persistence;
+using Shop.Domain.Entities;
+using Shop.Persistence.Context;
+using Shop.Persistence.Repositories.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly ShopDbContext _context;
+
+        public UserRepository(ShopDbContext context) : base(context)
+        {
+            _context = context
+        }
+    }
+}
