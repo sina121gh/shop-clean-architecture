c171cd713695f5be68b73e9749e64572624784d2
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-17 08:57:56 +0330

    addd user entity and its configuration

---
+++ Shop.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Salt { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Shop.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.HasIndex(u => u.UserName)
+                .IsUnique();
+
+            builder.HasIndex(u =>u.Email)
+                .IsUnique();
+
+            builder.Property(u => u.UserName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(u => u.Email)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(u => u.Salt)
+                .HasMaxLength(31)
+                .IsRequired();
+
+            builder.Property(u => u.Password)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}
