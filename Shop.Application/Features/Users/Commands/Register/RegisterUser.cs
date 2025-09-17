9eed77e50745c3d9088ed97afa181f1bf5eeb3e4
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-17 09:51:13 +0330

    added Register User Command and handler

---
+++ Shop.Application/Features/Users/Commands/Register/RegisterUser.cs
@@ -0,0 +1,48 @@
+﻿using ErrorOr;
+using MapsterMapper;
+using MediatR;
+using Shop.Application.Contracts.Persistence;
+using Shop.Application.Features.Users.Queries.GetById;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Commands.Create
+{
+    public class RegisterUserCommand : IRequest<ErrorOr<ShowUserDto>>
+    {
+        public RegisterUserDto RegisterUserDto { get; set; }
+    }
+
+    class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, ErrorOr<ShowUserDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public RegisterUserCommandHandler(IUserRepository userRepository,
+            IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ErrorOr<ShowUserDto>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = _mapper.Map<User>(request.RegisterUserDto);
+            await _userRepository.AddAsync(user);
+
+            try
+            {
+                await _userRepository.SaveChangesAsync();
+                return _mapper.Map<ShowUserDto>(user);
+            }
+            catch (Exception)
+            {
+                return Error.Unexpected("خطایی در ثبت اطلاعات رخ داد");
+            }
+        }
+    }
+}
---
+++ Shop.Application/Features/Users/Commands/Register/RegisterUserValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Shop.Application.Features.Users.Commands.Create;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Commands.Register
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator()
+        {
+            RuleFor(u => u.UserName)
+                .MaximumLength(20)
+                .NotEmpty();
+
+            RuleFor(u => u.Password)
+                .MaximumLength(100)
+                .NotEmpty();
+
+            RuleFor(u => u.Email)
+                .MaximumLength(100)
+                .NotEmpty()
+                .Matches(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")
+                .WithMessage("ایمیل را صحیح وارد نمایید");
+        }
+    }
+
+    class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(c => c.RegisterUserDto).SetValidator(new RegisterUserDtoValidator());
+        }
+    }
+}
