11bb7a1ecde27dc8d26fc9a95386e78f874c5d98
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-17 10:51:43 +0330

    added update user profile command and handler

---
+++ Shop.Application/Features/Users/Commands/UpdateProfile/UpdateProfile.cs
@@ -0,0 +1,55 @@
+﻿using ErrorOr;
+using MapsterMapper;
+using MediatR;
+using Shop.Application.Contracts.Persistence;
+using Shop.Application.DTOs.Category;
+using Shop.Application.Features.Users.Commands.Create;
+using Shop.Application.Features.Users.Queries.GetById;
+using Shop.Application.Persistence;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Commands.UpdateProfile
+{
+    public class UpdateProfileCommand : IRequest<ErrorOr<ShowUserDto>>
+    {
+        public int Id { get; set; }
+        public RegisterUserDto UserDto { get; set; }
+    }
+
+    class UpdateProfileCommandHandler : IRequestHandler<UpdateProfileCommand, ErrorOr<ShowUserDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateProfileCommandHandler(IUserRepository userRepository,
+            IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ErrorOr<ShowUserDto>> Handle(UpdateProfileCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+            if (user is null)
+                return Error.NotFound($"کاربر با آیدی {request.Id} یافت نشد");
+
+            _mapper.Map(request.UserDto, user);
+
+            try
+            {
+                await _userRepository.SaveChangesAsync();
+                return _mapper.Map<ShowUserDto>(user);
+            }
+            catch (Exception)
+            {
+                return Error.Unexpected("خطایی در ثبت اطلاعات رخ داد");
+            }
+        }
+    }
+}
---
+++ Shop.Application/Features/Users/Commands/UpdateProfile/UpdateProfileValdator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Shop.Application.Contracts.Persistence;
+using Shop.Application.Features.Categories.Commands.Create;
+using Shop.Application.Features.Users.Commands.Register;
+using Shop.Application.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Features.Users.Commands.UpdateProfile
+{
+    public class UpdateProfileValdator : AbstractValidator<UpdateProfileCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UpdateProfileValdator(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public UpdateProfileValdator()
+        {
+            RuleFor(c => c.Id)
+                .NotNull()
+                .GreaterThan(0)
+                .WithMessage("آیدی را صحیح وارد کنید")
+                .MustAsync(async (id, token) => await _userRepository.DoesExistByIdAsync(id))
+                .WithMessage("کاربر یافت نشد")
+                .WithErrorCode("404")
+                .DependentRules(() =>
+                {
+                    RuleFor(c => c.UserDto).SetValidator(new RegisterUserDtoValidator());
+                });
+        }
+    }
+}
