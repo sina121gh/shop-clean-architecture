58f319ea233a6eb76ceceff38de06736a8343d4d
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-18 09:43:31 +0330

    used PasswordHasher in users commands

--- Shop.Application/Features/Users/Commands/Register/RegisterUser.cs
+++ Shop.Application/Features/Users/Commands/Register/RegisterUser.cs
@@ -1,5 +1,6 @@
 ﻿using Shop.Application.Contracts.Persistence;
 using Shop.Application.Features.Users.Queries.GetById;
+using Shop.Application.Security;
 
 namespace Shop.Application.Features.Users.Commands.Create
 {
@@ -11,18 +12,25 @@ namespace Shop.Application.Features.Users.Commands.Create
     class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, ErrorOr<ShowUserDto>>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
         private readonly IMapper _mapper;
 
         public RegisterUserCommandHandler(IUserRepository userRepository,
+            IPasswordHasher passwordHasher,
             IMapper mapper)
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
             _mapper = mapper;
         }
 
         public async Task<ErrorOr<ShowUserDto>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
         {
             var user = _mapper.Map<User>(request.RegisterUserDto);
+            user.Salt = _passwordHasher.GenerateSalt();
+            user.Password = _passwordHasher.HashPassword(request.RegisterUserDto.Password, user.Salt);
+            user.IsAdmin = false;
+
             await _userRepository.AddAsync(user);
 
             try
--- Shop.Application/Features/Users/Commands/UpdateProfile/UpdateProfile.cs
+++ Shop.Application/Features/Users/Commands/UpdateProfile/UpdateProfile.cs
@@ -1,6 +1,7 @@
 ﻿using Shop.Application.Contracts.Persistence;
 using Shop.Application.Features.Users.Commands.Create;
 using Shop.Application.Features.Users.Queries.GetById;
+using Shop.Application.Security;
 
 namespace Shop.Application.Features.Users.Commands.UpdateProfile
 {
@@ -13,12 +14,15 @@ namespace Shop.Application.Features.Users.Commands.UpdateProfile
     class UpdateProfileCommandHandler : IRequestHandler<UpdateProfileCommand, ErrorOr<ShowUserDto>>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
         private readonly IMapper _mapper;
 
         public UpdateProfileCommandHandler(IUserRepository userRepository,
+            IPasswordHasher passwordHasher,
             IMapper mapper)
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
             _mapper = mapper;
         }
 
@@ -27,6 +31,8 @@ namespace Shop.Application.Features.Users.Commands.UpdateProfile
             var user = await _userRepository.GetByIdAsync(request.Id);
             if (user is null)
                 return Error.NotFound($"کاربر با آیدی {request.Id} یافت نشد");
+
+            user.Password = _passwordHasher.HashPassword(request.UserDto.Password, user.Salt);
 
             _mapper.Map(request.UserDto, user);
 
