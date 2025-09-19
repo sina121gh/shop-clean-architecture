6fee06da17d47022ffccd14d69ae67e1a23183ea
Alireza Ghiasy <sina121gh@gmail.com>
2025-09-19 10:39:39 +0330

    added put and delete endpoints for user

--- Shop.API/Controllers/UsersController.cs
+++ Shop.API/Controllers/UsersController.cs
@@ -1,11 +1,17 @@
-﻿using MediatR;
+﻿using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Shop.API.Extensions;
 using Shop.Application.Features.Categories.Queries.GetAll;
 using Shop.Application.Features.Products.Queries.GetAllProducts;
+using Shop.Application.Features.Users.Commands.Create;
+using Shop.Application.Features.Users.Commands.Delete;
+using Shop.Application.Features.Users.Commands.UpdateProfile;
 using Shop.Application.Features.Users.Queries.GetAll;
 using Shop.Application.Features.Users.Queries.GetById;
+using System.Security.Claims;
 
 namespace Shop.API.Controllers
 {
@@ -32,6 +38,35 @@ namespace Shop.API.Controllers
         public async Task<IActionResult> GetUserByIdAsync(int id)
         {
             var result = await _mediator.Send(new GetUserByIdQuery() { Id = id });
+
+            return this.ToActionResult(result);
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProfileAsync(int id, [FromBody] RegisterUserDto userDto)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            if (userId != id)
+            {
+                ErrorOr<Unit> unauthorizedResult = Error.Unauthorized();
+                return this.ToActionResult(unauthorizedResult);
+            }
+
+            var result = await _mediator.Send(new UpdateProfileCommand() { Id = userId, UserDto = userDto });
+
+            return this.ToActionResult(result);
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccountAsync(int id)
+        {
+            var userId = int.Parse(User.Identity?.Name);
+            if (userId != id)
+                return Unauthorized();
+
+            var result = await _mediator.Send(new DeleteUserCommand() { Id = userId});
 
             return this.ToActionResult(result);
         }
